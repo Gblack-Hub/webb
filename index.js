af113e90ab50333b201062674062368ba2c77a23
Olawale Oladiran <oladiranwale@gmail.com>
2021-10-30 08:45:34 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+// console.log("Wale");
+// alert('Hello');
+// prompt("What class is this?");
+// confirm("Are you in class?");
+
+// var data1 = 1234567809; //Number
+// var data2 = "qewretg"; //String
+// var isPresentInClass = false; //Boolean
+// var data3 = null; //Null
+// var data4; //undefined
+// console.log(typeof data4);
+
+// var tomi = {
+//     name: "Oluwatomi",
+//     complexion: "brown",
+//     hand: 2,
+//     leg: 2,
+//     head: 1,
+//     isPresentInClass: true,
+// }
+
+// var num = 2;
+// console.log(num++);
+// console.log(num+1);
+
+// var a = 5 + 2;
+// var b = 2 - 2;
+// var c = "";
+// var d = 0;
+
+
+// var one = "GoMy";
+// var two = "Code";
+
+// var result = one + two;
+// // var result = `${one}${two}`;
+
+// console.log(result);
+
+// var boolCheck = Boolean((5 < 2) && (2 == 2));
+// var typeCheck = typeof (d);
+
+// console.log(boolCheck);
+// console.log(typeCheck);
+// console.log(a === c);
+// console.log(!!7);
+
+// console.log(a, b);
+
+// a += b; //a = a + b;
+// a *= b; //a = a * b;
+
+// console.log(log)
+
+// var ourName = 8;
+
+// function addition() {
+
+// }
